fd40d3e93bb0a08bc5a2eb9f959bcca1e5c5cf36
mariellenolasco <marielle.nolasco@revature.com>
2021-05-06 13:10:23 -0700

    Added validation logic to Review model

--- 1-csharp/RestaurantReviews/RRModels/Review.cs
+++ 1-csharp/RestaurantReviews/RRModels/Review.cs
@@ -1,12 +1,26 @@
+using System;
 namespace RRModels
 {
     public class Review
     {
+        private int _rating;
         /// <summary>
         /// This describes the overall numeric rating of a restaurant
         /// </summary>
         /// <value></value>
-        public int Rating { get; set; }
+        public int Rating
+        {
+            get { return _rating; }
+            set
+            {
+                //Setting validation logic in properties
+                if (_rating < 0)
+                {
+                    throw new Exception("Rating should be greater tha zero.")
+                }
+                _rating = value;
+            }
+        }
         /// <summary>
         /// Verbose description of the dining experience
         /// </summary>
