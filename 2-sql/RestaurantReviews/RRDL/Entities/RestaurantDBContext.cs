7732fd3809fa91d16a5748304e5ff33968a6db22
mariellenolasco <marielle.nolasco@revature.com>
2021-05-12 11:46:01 -0700

    Scaffolded stuff

---
+++ 2-sql/RestaurantReviews/RRDL/Entities/RestaurantDBContext.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace RRDL.Entities
+{
+    public partial class RestaurantDBContext : DbContext
+    {
+        public RestaurantDBContext()
+        {
+        }
+
+        public RestaurantDBContext(DbContextOptions<RestaurantDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Restaurant> Restaurants { get; set; }
+        public virtual DbSet<Review> Reviews { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Restaurant>(entity =>
+            {
+                entity.ToTable("restaurants");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.City)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("city");
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("name");
+
+                entity.Property(e => e.State)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .HasColumnName("state");
+            });
+
+            modelBuilder.Entity<Review>(entity =>
+            {
+                entity.ToTable("reviews");
+
+                entity.Property(e => e.Id).HasColumnName("id");
+
+                entity.Property(e => e.Description)
+                    .IsRequired()
+                    .HasMaxLength(240)
+                    .HasColumnName("description");
+
+                entity.Property(e => e.Rating).HasColumnName("rating");
+
+                entity.Property(e => e.RestaurantId).HasColumnName("restaurantID");
+
+                entity.HasOne(d => d.Restaurant)
+                    .WithMany(p => p.Reviews)
+                    .HasForeignKey(d => d.RestaurantId)
+                    .HasConstraintName("FK__reviews__restaur__5EBF139D");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
