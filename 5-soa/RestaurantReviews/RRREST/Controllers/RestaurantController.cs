db6beead69eee1fc6d098b32e2f5f4a3276048d1
mariellenolasco <marielle.nolasco@revature.com>
2021-06-03 15:35:17 -0400

    Restaurant reviews rest api

---
+++ 5-soa/RestaurantReviews/RRREST/Controllers/RestaurantController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RRBL;
+using RRModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RRREST.Controllers
+{
+    [Route("api/Restuarants")]
+    [ApiController]
+    public class RestaurantController : ControllerBase
+    {
+        private readonly IRestaurantBL _restaurantBL;
+        public RestaurantController(IRestaurantBL restaurantBL)
+        {
+            _restaurantBL = restaurantBL;
+        }
+        // GET: api/<RestaurantController>
+        [HttpGet]
+        public IActionResult GetAllRestaurants()
+        {
+            // No more VMs yayyyyyy
+            // But, you may have a need to still come up with some version of your data to send across
+            // a network
+            // You'd call this version of your model a DTO (data transfer object) 
+            // You can use a DTO to aggregate data, leave out data that the client doesn't really need, etc
+            return Ok(_restaurantBL.GetAllRestaurants());
+        }
+
+        // GET api/<RestaurantController>/5
+        [HttpGet("{id}")]
+        public IActionResult GetRestaurantById(int id)
+        {
+            return Ok(_restaurantBL.GetRestaurantById(id));
+        }
+
+        // POST api/<RestaurantController>
+        [HttpPost]
+        public IActionResult AddNewRestaurant([FromBody] Restaurant newRestaurant)
+        {
+            return Created("api/Restaurant", _restaurantBL.AddRestaurant(newRestaurant));
+        }
+
+        // PUT api/<RestaurantController>/5
+        [HttpPut("{id}")]
+        public IActionResult UpdateRestaurant(int id, [FromBody] Restaurant updatedRestaurant)
+        {
+            _restaurantBL.UpdateRestaurant(updatedRestaurant);
+            return NoContent();
+        }
+
+        // DELETE api/<RestaurantController>/5
+        [HttpDelete("{id}")]
+        public IActionResult DeleteRestaurant(int id)
+        {
+            _restaurantBL.DeleteRestaurant(_restaurantBL.GetRestaurantById(id));
+            return NoContent();
+        }
+    }
+}
---
+++ 5-soa/RestaurantReviews/RRREST/Controllers/ReviewController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RRBL;
+using RRModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RRREST.Controllers
+{
+    [Route("api/Restaurants/{restaurantId}/Reviews")]
+    [ApiController]
+    public class ReviewController : ControllerBase
+    {
+        private readonly IReviewBL _reviewBL;
+        private readonly IRestaurantBL _restaurantBL;
+
+        public ReviewController(IReviewBL reviewBL, IRestaurantBL restaurantBL)
+        {
+            _reviewBL = reviewBL;
+            _restaurantBL = restaurantBL;
+        }
+
+        // GET: api/<ReviewController>
+        [HttpGet]
+        public IActionResult GetAllReviews(int restaurantId)
+        {
+            return Ok(_reviewBL.GetReviews(_restaurantBL.GetRestaurantById(restaurantId)));
+        }
+
+        // POST api/<ReviewController>
+        [HttpPost]
+        public IActionResult AddReview(int restaurantId, [FromBody] Review newReview)
+        {
+            return Created($"/api/Restaurants/{restaurantId}/Reviews",
+                _reviewBL.AddReview(
+                    _restaurantBL.GetRestaurantById(restaurantId),
+                    new Review(newReview.Rating, newReview.Description
+                    )));
+        }
+    }
+}
