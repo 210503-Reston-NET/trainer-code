6241c1fb7a2c11234cb19c103ad774b93160d41a
mariellenolasco <marielle.nolasco@revature.com>
2021-06-11 14:09:11 -0400

    added working unit tests to edit and get restaurant, the services

--- 6-angular/RRAngular/restaurantreviews/src/app/components/edit-restaurant/edit-restaurant.component.spec.ts
+++ 6-angular/RRAngular/restaurantreviews/src/app/components/edit-restaurant/edit-restaurant.component.spec.ts
@@ -1,16 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { restaurant } from 'src/app/models/restaurant';
+import { RestRevApiService } from 'src/app/services/restrevapi.service';
 
 import { EditRestaurantComponent } from './edit-restaurant.component';
 
 describe('EditRestaurantComponent', () => {
   let component: EditRestaurantComponent;
   let fixture: ComponentFixture<EditRestaurantComponent>;
-
+  class MockService {
+    EditRestaurant(restaurant: restaurant): Promise<restaurant> { return new Promise((resolve, reject) => { { } }) };
+  }
+  let restService: RestRevApiService;
+  let router: Router;
+  let route: ActivatedRoute;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ EditRestaurantComponent ]
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [EditRestaurantComponent],
+      providers: [{ provide: RestRevApiService, useClass: MockService }]
     })
-    .compileComponents();
+      .compileComponents();
+    restService = TestBed.inject(RestRevApiService);
+    router = TestBed.inject(Router);
+    route = TestBed.inject(ActivatedRoute);
   });
 
   beforeEach(() => {
